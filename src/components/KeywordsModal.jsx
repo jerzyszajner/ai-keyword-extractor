77bca8e0971a0448c375ac27026a996a7ed98913
jerzy szajner <jerzy.szajner@gmail.com>
2025-06-29 11:40:44 +0200

    fix modal issue on netlify

--- src/components/KeywordsModal.jsx
+++ src/components/KeywordsModal.jsx
@@ -13,27 +13,29 @@ import {
 
 const KeywordsModal = ({ isOpen, closeModal, keywords, isLoading }) => {
   return (
-    <>
-      <Modal isOpen={isOpen} onClose={closeModal}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Keywords</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody display="flex" alignItems="center" justifyContent="center">
-            {isLoading ? (
-              <CircularProgress isIndeterminate color="blue.300" />
-            ) : (
-              <Text>{keywords}</Text>
-            )}
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={closeModal}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
+    <Modal
+      isOpen={isOpen}
+      onClose={closeModal}
+      portalProps={{ appendToParentPortal: false }}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Keywords</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody display="flex" alignItems="center" justifyContent="center">
+          {isLoading ? (
+            <CircularProgress isIndeterminate color="blue.300" />
+          ) : (
+            <Text>{keywords}</Text>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={closeModal}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 };
 
