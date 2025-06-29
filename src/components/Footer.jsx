2cadf419e7f7d40328577e9b08e3ae7737496588
jerzy szajner <jerzy.szajner@gmail.com>
2025-06-29 11:35:36 +0200

    fix logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,21 @@
 import { Box, Image, Text, Flex } from "@chakra-ui/react";
-import logo from "../assets/openai.png";
+import logo from "../assets/cohere.svg";
 
 const Footer = () => {
   return (
-    <Box marginTop={50}>
-      <Flex justifyContent="center" alignItems="center">
-        <Image src={logo} alt="logo" marginRight={1} />
-        <Text>Powered by OpenAI</Text>
+    <Box marginTop={100}>
+      <Flex
+        flexDirection="column"
+        gap={2}
+        background="black"
+        border="1px solid #fcfcfc"
+        padding={3.5}
+        borderRadius={6}
+      >
+        <Text lineHeight={0.8} fontSize="0.8rem">
+          Powered by Cohere
+        </Text>
+        <Image src={logo} alt="logo" />
       </Flex>
     </Box>
   );
